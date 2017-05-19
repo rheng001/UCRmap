adff3eda37a0cd66fb7e94d5bfbb1d91e4d89468
Richard Heng <rheng001@gmail.com>
2017-05-19 03:26:36 -0700

    Updated

--- app/src/main/java/ucr/ucrmap/NavActivity.java
+++ app/src/main/java/ucr/ucrmap/NavActivity.java
@@ -26,6 +26,7 @@ public class NavActivity extends AppCompatActivity {
 
         Spinner mySpinner = (Spinner) findViewById(R.id.building);
 
+
         ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(NavActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.building_array));
         myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         mySpinner.setAdapter(myAdapter);
@@ -51,7 +52,7 @@ public class NavActivity extends AppCompatActivity {
             iconView.setLayoutParams(layoutParams);
         }
 
-
+        BottomNavigationViewHelper.disableShiftMode(bottomNavigationView);
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
