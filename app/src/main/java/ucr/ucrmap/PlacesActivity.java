b890776ee62147771c2ac8ed60b86a87663ba5a8
Richard Heng <rheng001@gmail.com>
2017-05-19 01:41:15 -0700

    Updated

--- app/src/main/java/ucr/ucrmap/PlacesActivity.java
+++ app/src/main/java/ucr/ucrmap/PlacesActivity.java
@@ -9,5 +9,7 @@ public class PlacesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_places);
+        overridePendingTransition(0, 0);
+
     }
 }
