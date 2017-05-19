33697a162516544cefd541204f83fb5b09beead4
Richard Heng <rheng001@gmail.com>
2017-05-19 00:22:42 -0700

    Updated

---
+++ app/src/main/java/ucr/ucrmap/NavActivity.java
@@ -0,0 +1,27 @@
+package ucr.ucrmap;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+public class NavActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_nav);
+
+        Spinner mySpinner = (Spinner) findViewById(R.id.building);
+
+        ArrayAdapter<String> myAdapter = new ArrayAdapter<String>(NavActivity.this, android.R.layout.simple_list_item_1, getResources().getStringArray(R.array.names));
+        myAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        mySpinner.setAdapter(myAdapter);
+    }
+
+
+}
