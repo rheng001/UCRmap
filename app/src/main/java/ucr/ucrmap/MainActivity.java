6f8f3d0b231c883327e91799ddce2f319dc024a5
Richard Heng <rheng001@gmail.com>
2017-05-18 21:30:38 -0700

    Updated

--- app/src/main/java/ucr/ucrmap/MainActivity.java
+++ app/src/main/java/ucr/ucrmap/MainActivity.java
@@ -1,57 +1,57 @@
 package ucr.ucrmap;
 
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
-import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.util.Log;
-import android.view.Menu;
-import android.widget.Toast;
-
-import com.here.android.mpa.common.GeoCoordinate;
-import com.here.android.mpa.common.OnEngineInitListener;
-import com.here.android.mpa.mapping.Map;
-import com.here.android.mpa.mapping.MapFragment;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-
+import android.content.Context;
 import android.content.Intent;
+import android.preference.PreferenceManager;
+import android.support.annotation.NonNull;
 import android.support.design.internal.BottomNavigationMenuView;
 import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends Activity {
-    private static final String LOG_TAG = MainActivity.class.getSimpleName();
+import org.osmdroid.api.IMapController;
+import org.osmdroid.config.Configuration;
+import org.osmdroid.tileprovider.tilesource.TileSourceFactory;
+import org.osmdroid.util.GeoPoint;
+import org.osmdroid.views.MapView;
 
-    // permissions request code
-    private final static int REQUEST_CODE_ASK_PERMISSIONS = 1;
+import ucr.ucrmap.AboutActivity;
+import ucr.ucrmap.NavActivity;
+import ucr.ucrmap.PlacesActivity;
+import ucr.ucrmap.R;
 
-    /**
-     * Permissions that need to be explicitly requested from end user.
-     */
-    private static final String[] REQUIRED_SDK_PERMISSIONS = new String[] {
-            Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.WRITE_EXTERNAL_STORAGE };
-
-    // map embedded in the map fragment
-    private Map map = null;
-
-    // map fragment embedded in this activity
-    private MapFragment mapFragment = null;
+public class MainActivity extends AppCompatActivity {
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        checkPermissions();
+        setContentView(R.layout.activity_main);
+
+        Context ctx = getApplicationContext();
+        //important! set your user agent to prevent getting banned from the osm servers
+        Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
+        setContentView(R.layout.activity_main);
+
+        MapView map = (MapView) findViewById(R.id.map);
+        map.setTileSource(TileSourceFactory.MAPNIK);
+
+
+        map.setMultiTouchControls(true);
+
+
+        IMapController mapController = map.getController();
+        mapController.setZoom(19);
+        GeoPoint startPoint = new GeoPoint(33.974942, -117.327270);
+        mapController.setCenter(startPoint);
+
+
 
         //BottomNavigationView bottomNavigationView = (BottomNavigationView)findViewById(R.id.bottom_navigation);
 
@@ -98,72 +98,5 @@ public class MainActivity extends Activity {
 
     }
 
-    private void initialize() {
-        setContentView(R.layout.activity_main);
-
-        // Search for the map fragment to finish setup by calling init().
-        mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.mapfragment);
-        mapFragment.init(new OnEngineInitListener() {
-            @Override
-            public void onEngineInitializationCompleted(OnEngineInitListener.Error error) {
-                if (error == OnEngineInitListener.Error.NONE) {
-                    // retrieve a reference of the map from the map fragment
-                    map = mapFragment.getMap();
-                    // Set the map center to the UCR
-                    map.setCenter(new GeoCoordinate(33.973782, -117.328069),
-                            Map.Animation.NONE);
-                    // Set the zoom level to the average between min and max
-                    map.setZoomLevel((60));
-                } else {
-                    Log.e(LOG_TAG, "Cannot initialize MapFragment (" + error + ")");
-                }
-            }
-        });
-    }
-
-    /**
-     * Checks the dynamically controlled permissions and requests missing permissions from end user.
-     */
-    protected void checkPermissions() {
-        final List<String> missingPermissions = new ArrayList<String>();
-        // check all required dynamic permissions
-        for (final String permission : REQUIRED_SDK_PERMISSIONS) {
-            final int result = ContextCompat.checkSelfPermission(this, permission);
-            if (result != PackageManager.PERMISSION_GRANTED) {
-                missingPermissions.add(permission);
-            }
-        }
-        if (!missingPermissions.isEmpty()) {
-            // request all missing permissions
-            final String[] permissions = missingPermissions
-                    .toArray(new String[missingPermissions.size()]);
-            ActivityCompat.requestPermissions(this, permissions, REQUEST_CODE_ASK_PERMISSIONS);
-        } else {
-            final int[] grantResults = new int[REQUIRED_SDK_PERMISSIONS.length];
-            Arrays.fill(grantResults, PackageManager.PERMISSION_GRANTED);
-            onRequestPermissionsResult(REQUEST_CODE_ASK_PERMISSIONS, REQUIRED_SDK_PERMISSIONS,
-                    grantResults);
-        }
-    }
-
-    @Override
-    public void onRequestPermissionsResult(int requestCode, @NonNull String permissions[],
-                                           @NonNull int[] grantResults) {
-        switch (requestCode) {
-            case REQUEST_CODE_ASK_PERMISSIONS:
-                for (int index = permissions.length - 1; index >= 0; --index) {
-                    if (grantResults[index] != PackageManager.PERMISSION_GRANTED) {
-                        // exit the app if one permission is not granted
-                        Toast.makeText(this, "Required permission '" + permissions[index]
-                                + "' not granted, exiting", Toast.LENGTH_LONG).show();
-                        finish();
-                        return;
-                    }
-                }
-                // all permissions were granted
-                initialize();
-                break;
-        }
-    }
 
 }
